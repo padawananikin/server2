f024bf1016c0b87b6ff69cf793d7ea6141e53671
yaashwaanth <gys24gva@gmail.com>
2025-09-09 09:27:47 +0530

    testing

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express()
+
+
+
+app.get("/",(req,res)=> {
+    res.send({
+        "msg":"Sever is working fine"
+    })
+})
+
+app.listen(4000,()=>{
+    console.log("Sever is listening to port 40000");
+    
+})
